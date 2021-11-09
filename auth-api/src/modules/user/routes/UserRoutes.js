3b56501d7b0b4d64939ac3ccc927b4a8890dd0c6
José Victor <jvictorfrance@outlook.com>
2021-11-08 23:35:31 -0300

    Desenvolvendo a autenticacao com JWT

---
+++ auth-api/src/modules/user/routes/UserRoutes.js
@@ -0,0 +1,12 @@
+import { Router } from "express";
+
+import UserController from "../controller/UserController.js";
+
+const router = new Router();
+
+router.get("/api/user/email/:email", UserController.findByEmail);
+router.post("/api/user/auth", UserController.getAccessToken);
+
+// /api/user/email/victorhugonegrisoli.css@gmail.com
+
+export default router;
